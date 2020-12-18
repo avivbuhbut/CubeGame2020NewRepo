e94a0349b3af92f615e2496dc6b62935a7a8d27a
avivbuhbut <avivb931@gmail.com>
2020-12-18 23:48:16 +0200

    18.12.20 23:48

---
+++ Assets/ConvertMoneyToTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConvertMoneyToTime : MonoBehaviour { 
+    public Button button;
+
+void OnEnable()
+{
+
+        button.onClick.AddListener(MyFunction);//adds a listener for when you click the button
+
+}
+void MyFunction()// your listener calls this function
+{
+        Debug.Log("this button is pressed!");
+}
+
+}
