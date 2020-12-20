ab7f566fd479b42ba3f44e6f1dbaa2d2cb5b84fd
avivbuhbut <avivb931@gmail.com>
2020-12-21 01:09:40 +0200

    21.12.20 01:09

---
+++ Assets/ContinueTimerAndOpenDoors.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class ContinueTimerAndOpenDoors : MonoBehaviour
+{
+
+    public Button button;
+
+
+    public static int CoutnerSecPlayerBuy = 0;
+    void OnEnable()
+    {
+
+        button.onClick.AddListener(MyFunction);//adds a listener for when you click the button
+
+    }
+
+
+    void MyFunction()// your listener calls this function
+    {
+        if (PlayerMoney.moneyCounter >= 6 && StartTimerEnterFirstChallange.PlayerPassThrow == false)
+        {
+
+            StartTimerEnterFirstChallange.PlayerPassThrow = true;
+
+
+        }
+
+    }
+}
