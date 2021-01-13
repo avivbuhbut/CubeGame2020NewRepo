2c2578b84ff75656bafa491c970ae87eb28738ab
avivbuhbut <avivb931@gmail.com>
2021-01-13 23:07:02 +0200

    13.01.21 23:06

---
+++ Assets/OpenManuOnPress.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenManuOnPress : MonoBehaviour
+{
+
+    public Button ConveyLogicButton;
+    public GameObject PanelGamObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        PanelGamObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void OnEnable()
+    {
+
+        ConveyLogicButton.onClick.AddListener(MyFunction);//adds a listener for when you click the button
+
+    }
+
+
+    void MyFunction()// your listener calls this function
+    {
+
+        PanelGamObj.SetActive(true);
+    }
+}
--- Assets/scripts/CreateBlocks.cs
+++ Assets/scripts/CreateBlocks.cs
@@ -6,7 +6,7 @@ public class CreateBlocks : MonoBehaviour
 {
 
     public static int BlocksAvilable;
-  //  public GameObject CubeGamObj;
+    //  public GameObject CubeGamObj;
     public GameObject CubeGamObj;
     public GameObject PizzaProtectorGamObj;
     private GameObject linehandler;
@@ -17,11 +17,11 @@ public class CreateBlocks : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       CubeGamObj.SetActive(false);
+        CubeGamObj.SetActive(false);
         PizzaProtectorGamObj.SetActive(false);
     }
 
-   
+
 
     void Update()
     {
@@ -49,8 +49,8 @@ public class CreateBlocks : MonoBehaviour
 
 
 
-            /*for debug only*/
-            if (Input.GetKey(KeyCode.Alpha5))
+        /*for debug only*/
+        if (Input.GetKey(KeyCode.Alpha5))
         {
             if (Input.GetMouseButtonDown(0))
             {
@@ -79,31 +79,31 @@ public class CreateBlocks : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-            {
-                CubeGamObj.SetActive(true);
-                mousepos = Input.mousePosition;
-                mousepos.z = 15.2f;
+                {
+                    CubeGamObj.SetActive(true);
+                    mousepos = Input.mousePosition;
+                    mousepos.z = 15.2f;
 
-                mousepos = Camera.main.ScreenToWorldPoint(mousepos);
-                linehandler = Instantiate(ObiSolverGamObj, mousepos, Quaternion.identity) as GameObject;
-                linehandler.gameObject.name = "ThrowPizzaObj";
-                linehandler.gameObject.tag = "ThrowPizzaObj";
+                    mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+                    linehandler = Instantiate(ObiSolverGamObj, mousepos, Quaternion.identity) as GameObject;
+                    linehandler.gameObject.name = "ThrowPizzaObj";
+                    linehandler.gameObject.tag = "ThrowPizzaObj";
 
 
 
-                linehandler.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationZ |
-                    RigidbodyConstraints.FreezePositionZ;
+                    linehandler.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationZ |
+                        RigidbodyConstraints.FreezePositionZ;
+                }
+
             }
 
-        }
 
 
 
 
+            if (Input.GetKey(KeyCode.Alpha7))
+            {
 
-        if (Input.GetKey(KeyCode.Alpha7))
-        {
-            
                 CubeGamObj.SetActive(true);
                 mousepos = Input.mousePosition;
                 mousepos.z = 15.2f;
@@ -118,66 +118,68 @@ public class CreateBlocks : MonoBehaviour
                 linehandler.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationZ |
                     RigidbodyConstraints.FreezePositionZ;
 
-            
-        }
 
-        /*create a wood block*/
-        if (Input.GetKey(KeyCode.B)) 
-        if (BlocksAvilable > 0 )
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
+            }
 
-                BlocksAvilable--; // every time createing a block
-                   GameObject.Find("Player").transform.GetComponent<BlocksAvilableTMPControl>().PlayerBlocksAvilableTMP.text = "X" + CreateBlocks.BlocksAvilable + " Blocks";
-                    CubeGamObj.SetActive(true);
-                mousepos = Input.mousePosition;
-                mousepos.z = 14.5f;
-
-                mousepos = Camera.main.ScreenToWorldPoint(mousepos);
-                linehandler = Instantiate(CubeGamObj, mousepos, Quaternion.identity) as GameObject;
-                    linehandler.gameObject.name = "CubePlayerCreate(Clone)";
-
-
-                    linehandler.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation |
-                        RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
-                }
-        }
-
-
-
-        /*delete a block*/
-        if (Input.GetKey(KeyCode.H))
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-              // every time createing a block
-
-
-                 	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);                    ///Very Useful Code! - tells the object name when clicked by the mouse
-                	RaycastHit hit;
-            	// Casts the ray and get the first game object hit
-                	Physics.Raycast(ray, out hit);
-   	                Debug.Log("This hit at " + hit.transform.name ); // till here
-
-                if (hit.transform.name == "CubePlayerCreate(Clone)" || hit.transform.name == "PizzaPanelProtector(Clone)")
+            /*create a wood block*/
+            if (Input.GetKey(KeyCode.B))
+                if (BlocksAvilable > 0)
                 {
+                    if (Input.GetMouseButtonDown(0))
+                    {
 
-                    BlocksAvilable++;
-                    hit.transform.gameObject.SetActive(false);
-                    GameObject.Find("Player").transform.GetComponent<BlocksAvilableTMPControl>().PlayerBlocksAvilableTMP.enabled = true;
-                GameObject.Find("Player").transform.GetComponent<BlocksAvilableTMPControl>(). PlayerBlocksAvilableTMP.text = "X" + CreateBlocks.BlocksAvilable + " Blocks";
+                        BlocksAvilable--; // every time createing a block
+                        GameObject.Find("Player").transform.GetComponent<BlocksAvilableTMPControl>().PlayerBlocksAvilableTMP.text = "X" + CreateBlocks.BlocksAvilable + " Blocks";
+                        CubeGamObj.SetActive(true);
+                        mousepos = Input.mousePosition;
+                        mousepos.z = 14.5f;
+
+                        mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+                        linehandler = Instantiate(CubeGamObj, mousepos, Quaternion.identity) as GameObject;
+                        linehandler.gameObject.name = "CubePlayerCreate(Clone)";
+
+
+                        linehandler.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation |
+                            RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+                    }
+                }
+
+
+
+            /*delete a block*/
+            if (Input.GetKey(KeyCode.H))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    // every time createing a block
+
+
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);                    ///Very Useful Code! - tells the object name when clicked by the mouse
+                	RaycastHit hit;
+                    // Casts the ray and get the first game object hit
+                    Physics.Raycast(ray, out hit);
+                    Debug.Log("This hit at " + hit.transform.name); // till here
+
+                    if (hit.transform.name == "CubePlayerCreate(Clone)" || hit.transform.name == "PizzaPanelProtector(Clone)")
+                    {
+
+                        BlocksAvilable++;
+                        hit.transform.gameObject.SetActive(false);
+                        GameObject.Find("Player").transform.GetComponent<BlocksAvilableTMPControl>().PlayerBlocksAvilableTMP.enabled = true;
+                        GameObject.Find("Player").transform.GetComponent<BlocksAvilableTMPControl>().PlayerBlocksAvilableTMP.text = "X" + CreateBlocks.BlocksAvilable + " Blocks";
+                    }
                 }
             }
+
+            //CubeGamObj.GetComponent<Rigidbody>().isKinematic = false;
         }
 
-        //CubeGamObj.GetComponent<Rigidbody>().isKinematic = false;
+
+        void OnCollisionEnter(Collision collision)
+        {
+            //if(collision.gameObject.transform.tag == "Floor")
+            //   Instantiate(CubeGamObj, new Vector3(this.transform.position.x, 0, 0), Quaternion.identity);
+        }
     }
 
-
-    void OnCollisionEnter(Collision collision)
-    {
-        //if(collision.gameObject.transform.tag == "Floor")
-         //   Instantiate(CubeGamObj, new Vector3(this.transform.position.x, 0, 0), Quaternion.identity);
-    }
 }
