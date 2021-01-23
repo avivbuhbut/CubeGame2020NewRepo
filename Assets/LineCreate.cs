6fdb480d89f044a7d8f6bbaf649b96e75c64ab4f
avivbuhbut <avivb931@gmail.com>
2021-01-23 22:24:59 +0200

    23.01.21 22:24

--- Assets/LineCreate.cs
+++ Assets/LineCreate.cs
@@ -57,41 +57,42 @@ public class LineCreate : MonoBehaviour
             LineRender.SetPosition(0, ConveyorStartTrans.transform.position);
             LineRender.SetPosition(1, PlayerPos.transform.position);
         }
-  
+
         if (hitConvyorEnd)
         {
             LineRender.SetPosition(1, ConveyorEndTrans.transform.position);
             // LineRender.SetPosition(1)
-      
+
             float _Distance = Vector3.Distance(ConveyorStartTrans.position, ConveyorEndTrans.position);
             if (counter == 0)
             {
-          
-                 ConveyorStach =   Instantiate(StarchConeyorTrans.gameObject, new Vector3(ConveyorStartTrans.position.x-5, ConveyorStartTrans.position.y, ConveyorStartTrans.position.z), Quaternion.identity);
-              
+
+                ConveyorStach = Instantiate(StarchConeyorTrans.gameObject, new Vector3(ConveyorStartTrans.position.x - 5, ConveyorStartTrans.position.y, ConveyorStartTrans.position.z), Quaternion.identity);
+
                 counter++;
             }
 
-
+            
             //scaling with the object
-            /*
-            Vector3 centerPos = new Vector3(ConveyorStartTrans.position.x + ConveyorEndTrans.transform.position.x, ConveyorStartTrans.position.y +
-                 ConveyorEndTrans.transform.position.y) / 2f;
 
-            float scaleX = Vector3.Distance(new Vector3(ConveyorStartTrans.position.x, 0, 0), new Vector3(ConveyorEndTrans.transform.position.x, 0, 0));
-            float scaleY = Vector3.Distance(new Vector3(0, ConveyorStartTrans.position.y, 0), new Vector3(0, ConveyorEndTrans.transform.position.y, 0));
-            ConveyorStach.transform.position = centerPos;
-            ConveyorStach.transform.localScale = new Vector3(scaleX, scaleY, 1);
-            */
+        //    Vector3 centerPos = new Vector3(ConveyorStartTrans.position.x + ConveyorEndTrans.transform.position.x, ConveyorStartTrans.position.y +
+         //       ConveyorEndTrans.transform.position.y) / 2f;
+
+     //    float scaleX = Vector3.Distance(new Vector3(ConveyorStartTrans.position.x, 0, 0), new Vector3(ConveyorEndTrans.transform.position.x, 0, 0));
+     ////       float scaleY = Vector3.Distance(new Vector3(0, ConveyorStartTrans.position.y, 0), new Vector3(0, ConveyorEndTrans.transform.position.y, 0));
+     //       ConveyorStach.transform.position = centerPos;
+      //    ConveyorStach.transform.localScale = new Vector3(scaleX, 2, 1);
+
+    
 
 
-        //need to be a combination of these two
+            //need to be a combination of these two
 
-            /*
-              ConveyorStach.transform.localScale = new Vector3(Mathf.Abs(ConveyorStartTrans.position.x - ConveyorEndTrans.position.x), 0.1f, LineRender.startWidth);
-              ConveyorStach.transform.position = new Vector3((ConveyorStartTrans.position.x + ConveyorEndTrans.position.x), (ConveyorStartTrans.position.y + ConveyorEndTrans.position.y)+2, LineRender.GetPosition(1).z);
-             //ConveyorStach.transform.rotation = new Vector3();
-             */
+
+             ConveyorStach.transform.localScale = new Vector3(Mathf.Abs(ConveyorStartTrans.position.x - ConveyorEndTrans.position.x), 0.1f, LineRender.startWidth);
+            ConveyorStach.transform.position = new Vector3((ConveyorStartTrans.position.x + ConveyorEndTrans.position.x), (ConveyorStartTrans.position.y + ConveyorEndTrans.position.y) + 2, LineRender.GetPosition(1).z);
+            ConveyorStach.transform.localRotation = Quaternion.Euler(0, 0, 0); // try to play with that a bit
+
         }
 
     }
