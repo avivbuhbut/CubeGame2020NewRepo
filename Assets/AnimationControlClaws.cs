dc31b85a9e0cacf1eea187c9f83a672416995021
avivbuhbut <avivb931@gmail.com>
2021-02-05 22:54:53 +0200

    05.02.21 22:54

---
+++ Assets/AnimationControlClaws.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControlClaws : MonoBehaviour
+{
+
+  public  Animation ClawCubeWithANim;
+    public Animation LowerCLaw;
+    // Start is called before the first frame update
+    void Start()
+    {
+      ClawCubeWithANim.Stop("CloseClaw");
+       // ClawCubeWithANim.Stop("LowerClaw");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ClawCubeWithANim.Play("CloseClaw");
+        
+
+        }
+
+
+        
+
+
+        // if (Input.GetKeyDown(KeyCode.UpArrow))
+        // {
+        //    ClawCubeWithANim.Play("LowerClaw");
+
+        //  }
+
+    }
+}
