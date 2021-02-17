fa9daeeaf27e7192d47807e90a60d809f5aba681
avivbuhbut <avivb931@gmail.com>
2021-02-17 14:11:22 +0200

    17.02.21 14:13

---
+++ Assets/HitByPowerCell.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitByPowerCell : MonoBehaviour
+{
+    bool ColidedPowerCircle;
+    int counterPowerCellHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ColidedPowerCircle&& counterPowerCellHit<9)
+            this.transform.GetComponent<Renderer>().material.color = Color.Lerp(Color.white, Color.red, Mathf.PingPong(Time.time, 1));
+
+        if (counterPowerCellHit == 9)
+        {
+            this.transform.GetComponent<Renderer>().material.color = Color.green;
+
+        }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.name == "PowerCircle(Clone)")
+        {
+            counterPowerCellHit++;
+            ColidedPowerCircle = true;
+            collision.transform.gameObject.SetActive(false);
+        }
+
+    }
+}
