a29f370739139ce757f94891a5dcd98e11bf458d
avivbuhbut <avivb931@gmail.com>
2021-03-03 09:15:03 +0200

    03.03.21 09:14

--- Assets/PersonalBestTimeStageOne.cs
+++ Assets/PersonalBestTimeStageOne.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 using TMPro;
 using UnityEditor;
 using System.IO;
+
 public class PersonalBestTimeStageOne : MonoBehaviour
 {
     // Start is called before the first frame update
+    public TextMeshPro NewBestScore;
+    public TextAsset ScoreFile;
     public TextMeshPro PeronalBest;
     int Min;
+    Color lerpedColor;
+    bool newBestScore;
     public static float TotalTime;
                int counter = 0;
+    private string theWholeFileAsOneLongString;
+    private List<string> eachLine;
+    bool PresetTimer;
     void Start()
     {
-        PeronalBest.gameObject.SetActive(true);
+        NewBestScore.gameObject.SetActive(false);
+        PeronalBest.gameObject.SetActive(false);
+        theWholeFileAsOneLongString = ScoreFile.text;
+
+        eachLine = new List<string>();
+        eachLine.AddRange(
+                    theWholeFileAsOneLongString.Split("\n"[0]));
+  //      PeronalBest.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -25,22 +40,39 @@ public class PersonalBestTimeStageOne : MonoBehaviour
         TotalTime +=0.4f * Time.deltaTime;
        if (OpenDoorStageOne.PlayerFinishLevel)
        {
+            PeronalBest.gameObject.SetActive(true);
             if (TotalTime % 60 > 10 && TotalTime > 0)
-                PeronalBest.text =  Min + ":" + (int)TotalTime % 60;
+                PeronalBest.text = Min + ":" + (int)TotalTime % 60;
             else if (TotalTime % 60 < 10 && TotalTime > 0)
             {
-                PeronalBest.text =+ Min + ":" + "0" + (int)TotalTime % 60;
+                PeronalBest.text = +Min + ":" + "0" + (int)TotalTime % 60;
                 //  PeronalBest.text = TotalTime + "";
             }
 
             if (counter == 0)
          {
-             WriteData(TotalTime);
+
+                if (int.Parse(eachLine[0]) > TotalTime)
+                {
+                    NewBestScore.gameObject.SetActive(true);
+                    NewBestScore.text = "New Best Score!";
+                    //eachLine[0] = TotalTime +"";
+                    newBestScore = true;
+                    WriteData(TotalTime);
+
+                }
              counter = 1;
          }
 
-        }
+            if (newBestScore)
+            {
+                lerpedColor = Color.Lerp(Color.black, NewBestScore.transform.GetComponent<Renderer>().material.color, Mathf.PingPong(Time.time, 1));
+                NewBestScore.transform.GetComponent<Renderer>().material.color = lerpedColor;
+            }
 
+        }
+    //    Debug.Log("eachLine[0]:" + eachLine[0]);
+   //     Debug.Log("theWholeFileAsOneLongString:" + theWholeFileAsOneLongString);
     }
 
 
@@ -63,8 +95,14 @@ public class PersonalBestTimeStageOne : MonoBehaviour
 
     void WriteData(float TotalTime)
     {
+        File.WriteAllText("MyPath.txt", "");
         StreamWriter writer = new StreamWriter("MyPath.txt", true);
+  
+       // File.WriteAllText("MyPath.txt", TotalTime + "");
         writer.Write(TotalTime);
         writer.Close();
     }
+
+
+    
 }
