13a7c5c1eff8d1f041f8c7749a63d43334f4c8b9
avivbuhbut <avivb931@gmail.com>
2021-05-07 20:18:08 +0300

    07.05.21 20:17

--- Assets/scripts/PlayerMovment.cs
+++ Assets/scripts/PlayerMovment.cs
@@ -7,19 +7,41 @@ public class PlayerMovment : MonoBehaviour
     // Use this for initialization
     Rigidbody rigidbody;
     bool PlayerTouchingTheFloor;
-
+    public PhysicMaterial DefaultBounciessColider;
+    float timerDownA;
+    float TimerUpA;
+    bool PreddesA;
     void Start()
     {
-  
+        
     }
 
+
+    void Update()
+    {
+
+     //   if (Input.GetKey(KeyCode.Space))
+//this.transform.GetComponent<CapsuleCollider>().material.bounciness = 0;
+
+      //  if (Input.GetKey(KeyCode.LeftControl)) { 
+   
+      //      this.transform.GetComponent<CapsuleCollider>().material.bounciness = 0;
+      //      Debug.Log("Here");
+     //   }
+    //    else
+      //      this.transform.GetComponent<CapsuleCollider>().material = DefaultBounciessColider;
+       //his.transform.GetComponent<CapsuleCollider>().material.bounciness = 1;
+
+    }
     // Update is called once per frame
-     void FixedUpdate()
+    void FixedUpdate()
     {
 
 
-        if (this.transform.GetComponent<Rigidbody>().velocity.magnitude > 10)
-            this.transform.GetComponent<PlayerMovment>().enabled = false;
+        //  if (this.transform.GetComponent<Rigidbody>().velocity.magnitude > 10)
+        //     this.transform.GetComponent<PlayerMovment>().enabled = false;
+
+    
 
         PlayerMovmentCotrol();
     }
@@ -30,22 +52,54 @@ public class PlayerMovment : MonoBehaviour
 
 
 
-      
-      
+
+
 
 
         if (Input.GetKey(KeyCode.Space))
             {
-                transform.Translate(Vector3.up * Time.deltaTime * 8f, Space.Self);
+                transform.Translate(Vector3.up * Time.deltaTime * 0.8f, Space.Self);
                 this.transform.GetComponent<Rigidbody>().mass = 0.3f;
 
-            }
-            //     rigidbody.velocity = new Vector2(rigidbody.velocity.x, speed);
+            this.transform.GetComponent<CapsuleCollider>().material = DefaultBounciessColider;
+        }else
+            this.transform.GetComponent<CapsuleCollider>().material.bounciness = 0;
+        //     rigidbody.velocity = new Vector2(rigidbody.velocity.x, speed);
 
-            if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
             {
+            PreddesA = true;
+            TimerUpA += 0.8f * Time.deltaTime;
                 transform.Translate(Vector3.left * Time.deltaTime * speed, Space.Self); //LEFT
+        }
+        else { 
+        timerDownA = TimerUpA;
+        float TempSped = speed;
+            if (PreddesA)
+            {
+                timerDownA -= 0.8f * Time.deltaTime;
+
+                Debug.Log("timerDownA: " + timerDownA + "TempSped" + TempSped);
+
+
+                if (timerDownA % 1 == 0)
+                {
+                    if (TempSped > 0)
+                    {
+                        TempSped -= 1;
+                        transform.Translate(Vector3.left * Time.deltaTime * TempSped, Space.Self); //LEFT
+                    }
+                    else
+                    {
+                        PreddesA = false;
+                    }
+                }
+
+
+
             }
+            }
+
             if (Input.GetKey(KeyCode.D))
             {
                 transform.Translate(Vector3.right * Time.deltaTime * speed, Space.Self); //RIGHT
